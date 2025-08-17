266388180d090927c08654e9c65c5f9fe5cec44f
WisemanPK <stevekgotsola@gmail.com>
2025-08-17 20:46:51 +0200

    R/P/S.py

--- editpdf.py
+++ editpdf.py
@@ -1,12 +1,50 @@
-from PyPDF2 import PdfReader, PdfWriter
+import random
+emojis = {
+    'r': '🪨',
+    'p': '📜',
+    's': '✂️'
+}
+choices = ('r', 'p', 's')
 
-reader = PdfReader("input.pdf")
-writer = PdfWriter()
 
-# Copy all pages except first
-for page in reader.pages[1:]:
-    writer.add_page(page)
+def get_user_choice():
+    while True:
+        user_choice = input('Rock, Paper, or Scissor? (r/p/s) ').lower()
+        if user_choice in choices:
+            return user_choice
+        else:
+            print("Invalid Choice!")
 
-# Save new file
-with open("edited.pdf", "wb") as f:
-    writer.write(f)
+
+def display_choices(user_choice, computer_choice):
+    print(f'You chose {emojis[user_choice]}')
+    print(f'Computer chose {emojis[computer_choice]}')
+
+
+def determine_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        print("Tie!")
+    elif ((user_choice == 'r' and computer_choice == 's') or
+          (user_choice == 's' and computer_choice == 'p') or
+          (user_choice == 'p' and computer_choice == 'r')):
+        print("You beat the Machine..You win!")
+    else:
+        print("You lost to a machine!")
+
+
+def play_game():
+    while True:
+        user_choice = get_user_choice()
+
+        computer_choice = random.choice(choices)
+
+        display_choices(user_choice, computer_choice)
+
+        determine_winner(user_choice, computer_choice)
+
+        should_cont = input("Continue? (y/n) ").lower()
+        if should_cont == 'n':
+            break
+
+
+play_game()
